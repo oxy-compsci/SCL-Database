dcee46d0e3df54d74cbb8bf6032948d69a9348fa
cal <caelan.barr@gmail.com>
2016-09-30 16:35:33 -0700

    In this update a few things were updated: 1. I fixed the function now titled, "run_image(path)" and made it so (1) it properly creates the text files and moves the finished files to the completed folder, and (2) the current naming system has now been updated so that completed image files are named document(1,2,3...)image, and the created text files are named document(1,2,3)text. 2. I made some style changes such as cleaning up my descriptions for functions, giving the fixed variables capital first letters, and spacing everything more cleanly. 3. I added "filler" files to each folder as git does not allow empty folders.
    if you'd like to test it out, empty out the "completed_files" folder and the "completed_text_files" folder minus the "filler" files. Then go ahead and put any images in the loading zone and run the program.
    
    more to come!

--- scl_python.py
+++ scl_python.py
@@ -15,18 +15,22 @@ from os.path import dirname, realpath
 import random
 
 from flask import Flask, render_template, send_from_directory
+import glob
 
 app = Flask(__name__)
 
-# Creating variables for the location
-# of the image loading zone, completed
-# files, and text files
-scl_loading_zone = "test_loading_zone"
-textpath = "test_text_files"
-dest_path = "test_completed_files"
+
+# FOLDER LOCATIONS
 
 
-# checks a specific folder and finds out if it is empty or not
+Loading_zone = "loading_zone"
+Text_path = "completed_text_files"
+Dest_path = "completed_files"
+
+
+# CHECKS FOR FILES IN FOLDER
+
+
 def file_check(path):
     files_no_folders = [f for f in listdir(path) if isfile(join(path, f))]
     if len(files_no_folders) <= 1:
@@ -34,65 +38,79 @@ def file_check(path):
     else:
         return True
 
-# takes an image, returns text based on OCR
+
+# IMAGE TO OCR STRING
+
+
 def ocr_extract(file):
     ocr_text = pytesseract.image_to_string(Image.open(file))
     return ocr_text
 
-# takes a string, file name, and path,
-# creates a .txt file with the string
-# as the body, and places it at end of path
-def text_file_creator(string, filename, textpath):
-    new_file = open(os.path.join(textpath, filename), "w")
+
+# STRING TO TEXT FILE CREATOR
+
+
+def text_file_creator(string, filename, path):
+    new_file = open(os.path.join(path, filename), "w")
     new_file.write(string)
 
-# Puts it together -
-# images in folder,
-# OCR on images,
-# create text file in different folder,
-# original image gets moved to another folder.
-def jpg_to_text(path):
+
+# IMAGE CONVERTER USING ABOVE FUNCTIONS
+
+def run_image(path):
     count = -1
-    if file_check(path) is True:
+    if file_check(path):
         for filename in os.listdir(path):
-            if filename.endswith(".JPG" or ".JPEG" or ".PNG" or ".GIF" or ".TIF"):
+            print(filename)
+            if filename.endswith(".jpg" or ".jpeg" or ".png" or ".gif" or ".tif"):
+                filename = os.path.join(path, filename)
                 count += 1
-                text_name = "image" + str(count)
-                text = ocr_extract(os.path.join(path, filename))
-                text_file_creator(text, text_name, textpath)
-                shutil.move(os.path.join(path, filename), dest_path)
+                text_name = "document" + str(count) + "text"
+                image_rename = os.path.join(Dest_path, "document" + str(count) + "image")
+                text = ocr_extract(filename)
+                text_file_creator(text, text_name, Text_path)
+                print("renaming {} to {}".format(filename, image_rename))
+                os.rename(filename, image_rename)
 
-jpg_to_text(scl_loading_zone)
 
-def get_data():
+
+run_image(Loading_zone)
+
+
+# DESCRIPTION NEEDED
+
+
+def get_img_filenames():
     file_names = []
-    path = "test_completed_files"
+    path = "completed_files"
     dirs = os.listdir( path )
     for file in dirs:
         file_names.append(file)
     return file_names
 
-def get_text_data():
+
+# DESCRIPTION NEEDED
+
+
+def get_txt_filenames():
     text_file_names = []
-    path = 'test_text_files'
+    path = 'completed_text_files'
     dirs = os.listdir(path)
     for file in dirs:
         text_file_names.append(file)
     return text_file_names
 
 
-
-
-
 @app.route('/')
 def display_homepage():
-    return render_template('home.html', file_names=get_data())
+    return render_template('home.html', file_names=get_img_filenames())
 
-# make a lopp that goes through all the files in the folder, then adds them to the page
+
+# make a loop that goes through all the files in the folder, then adds them to the page
 @app.route('/<file_name>')
 def display_images(file_name):
-    image_file_names = get_data()
-    text_file_names = get_text_data()
+    image_file_names = get_img_filenames()
+    text_file_names = get_txt_filenames()
     nested_list = list(zip(image_file_names, text_file_names))
     length = len(nested_list)
     for index in range(length):
@@ -100,9 +118,10 @@ def display_images(file_name):
     return render_template('image.html', image_file_name=pairing[0], text_file_name=pairing[1])
 
 
-@app.route('/test_completed_files/<file_name>')
+@app.route('/completed_files/<file_name>')
 def image_file(file_name):
-    return send_from_directory('test_completed_files', file_name)
+    return send_from_directory('completed_files', file_name)
+
 
 '''
     length = len(students)
@@ -117,6 +136,7 @@ def image_file(file_name):
                 next_student = students[index + 1]
 '''
 
+
 '''
 @app.route('/')
 def display_homepage():
@@ -127,18 +147,17 @@ def display_homepage():
 if __name__ == '__main__':
     app.run(debug=True)
 #
-#@app.route('/test_completed_files/<file>')
+#@app.route('/completed_files/<file>')
 #def uploaded_file(file):
 #    return render_template('base.html', file=file)
 
-#@app.route('/test_completed_files/<file>')
+#@app.route('/completed_files/<file>')
 #def send_file(file):
-#    return send_from_directory('test_completed_files', file)
+#    return send_from_directory('completed_files', file)
 
 
-# THIS IS A TEST!
-# PART 2
 '''
+
 
 if __name__ == "__main__":
     app.run(debug=True)
