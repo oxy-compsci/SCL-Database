cc1e400e7a74a3f23cd2eb69903969946c06563b
cal <caelan.barr@gmail.com>
2016-10-21 11:04:51 -0700

    Started to build the metadata section. Automatically adds metadata categories for each image that is uploaded. Naming system now allows for multiple runs.

--- scl_python.py
+++ scl_python.py
@@ -2,15 +2,13 @@ try:
     import Image
 except ImportError:
     from PIL import Image
-import pytesseract
-
-import os, sys
+import os
+import re
 from os import listdir
 from os.path import isfile, join
-import re
 
-from flask import Flask, render_template, send_from_directory, request
-
+import pytesseract
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -37,21 +35,93 @@ def text_file_creator(string, filename, path):
     new_file = open(os.path.join(path, filename), "w")
     new_file.write(string)
 
-# [ENTER DESCRIPTION HERE]
+# ADDS A SET OF METADATA CATEGORIES FOR A FILENAME
 def write_metadata_file(file_name):
     file = open('metadata.txt', 'a')
-    file.write('\n\nFile Name: {}\nBox Number: []\nDate Added (mm/dd/yyyy): []\nName of Uploader (Last, First): []'
+    file.write('\n\nFile Name: [{}]\nBox Number: []\nDate Added (mm/dd/yyyy): []\nName of Uploader (Last, First): []'
                '\nComments/Notes about File: []'.format(str(file_name)))
     file.close()
 
-# RETURNS FILE COUNT LOCATED IN METADATA
-def next_file_name():
+# POSSIBLE NEW SOLUTION IN FUNCTION BELOW
+# ------------------------------------------
+# # RETURNS FILE COUNT LOCATED IN METADATA
+# def next_file_number():
+#     metadata = open('metadata.txt', 'r')
+#     metastring = metadata.read()
+#     find_counter = re.search('File Counter = (.*)\n', metastring)
+#     filenumber = find_counter.group(1)
+#     return filenumber
+# next_file_number()
+# ------------------------------------------
+
+# RETURNS FILE COUNT LOCATED IN FILECOUNT.TXT
+def check_file_number():
+    string = open('filecount.txt', 'r')
+    number = string.read()
+    return number
+
+# ADDS ONE TO FILE COUNT IN FILECOUNT.TXT
+def count_plus_one():
+    current_num = check_file_number()
+    current_num = int(current_num)
+    next_num = current_num + 1
+    update = open('filecount.txt', 'w')
+    update.write(str(next_num))
+
+# RETURNS TRUE IF FILE NAME ALREADY EXISTS IN METADATA, FALSE IF NOT.
+def metacheck(filenum):
     metadata = open('metadata.txt', 'r')
     metastring = metadata.read()
-    find_counter = re.search('File Counter = (.*)\n', metastring)
-    filenumber = find_counter.group(1)
-    return filenumber
-next_file_name()
+    filenum = str(filenum)
+    find_filename = metastring.find('document' + filenum + 'image')
+    if find_filename is not -1:
+        return True
+    else:
+        return False
+
+# FINDS METADATA FOR FILE NAME, RETURNS NESTED LIST OF CATEGORY INPUTS
+def pull_metadata(filenum):
+    if metacheck(filenum) is True:
+        metadata = open('metadata.txt', 'r')
+        metastring = metadata.read()
+        filenum = str(filenum)
+        find_filename = metastring.find('document' + filenum + 'image')
+        print(find_filename)
+        current_location = find_filename - 12
+        lines = []
+        open_index = []
+        close_index = []
+        for char in metastring[current_location:]:
+            if len(lines) <= 5:
+                # print('loop is running over character: {}'.format(char))
+                if char is '[':
+                    print('I found an open bracket!')
+                    print('i promise {} is an opening bracket'.format(char))
+                    open_index.append(metastring.index(char))
+                    print(open_index)
+
+                if char is ']':
+                    print('I found the closed bracket')
+                    print('i promise {} is a closing bracket'.format(char))
+                    close_index.append(metastring.index(char))
+                    print(close_index)
+                    lines.append(metastring[open_index[0]:close_index[0]])
+                    current_location += close_index[0] + 1
+
+        return(lines)
+print(pull_metadata(12))
+
+
+
+
+
+
+        # while len(lines) <= 5:
+        #     metastring.splitlines[current_line:]()
+
+
+print(pull_metadata('12'))
+
 
 # RETURNS LIST OF FILE NAMES IN COMPLETED FILES FOLDER
 def get_img_filenames():
@@ -75,23 +145,20 @@ def get_txt_filenames():
 # USES TEXT_FILE_CREATOR TO CREATE TEXT FILES FOR OCR STRINGS,
 # MOVES COMPLETED IMAGES AND RESPECTIVE TEXT FILES TO PROPER FOLDERS
 def run_image(path):
-    count = -1
     if file_check(path):
         for filename in os.listdir(path):
-            print("checking if {} is an image file".format(filename))
             if filename.endswith(".jpg" or ".jpeg" or ".png" or ".gif" or ".tif"):
                 print("{} is an image file".format(filename))
                 filename = os.path.join(path, filename)
-                count += 1
+                count = check_file_number()
                 text_name = "document" + str(count) + "text"
-                print('this is the text name: {}'.format(text_name))
                 image_title = "document" + str(count) + "image"
                 image_file_dest = os.path.join(Dest_path, image_title)
                 write_metadata_file(image_title)
                 text = ocr_extract(filename)
                 text_file_creator(text, text_name, Text_path)
-                print("renaming {} to {}".format(filename, image_file_dest))
                 os.rename(filename, image_file_dest)
+                count_plus_one()
             else:
                 print("{} is not an image file".format(filename))
 
